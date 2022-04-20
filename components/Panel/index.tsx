eec40702ff7bb027311883db42eb9251f23b1e29
Felix Erdmann <hello@felixerdmann.com>
2022-04-20 17:44:43 +0200

    initial dnd

--- components/Panel/index.tsx
+++ components/Panel/index.tsx
@@ -1,10 +1,16 @@
 import { Disclosure, Transition } from '@headlessui/react'
 import { ChevronUpIcon, MenuIcon, TrashIcon } from '@heroicons/react/outline'
 import clsx from 'clsx'
+import { Draggable } from 'react-beautiful-dnd'
+import { resetServerContext } from 'react-beautiful-dnd'
+
+resetServerContext()
 
 type PanelProps = {
   children: React.ReactElement
   type?: keyof typeof types
+  draggableId: string
+  draggableIndex: number
 }
 
 const types = {
@@ -20,53 +26,66 @@ const types = {
   },
 }
 
-const Panel = ({ children, type = 'room' }: PanelProps) => {
+const Panel = ({
+  children,
+  type = 'room',
+  draggableId,
+  draggableIndex,
+}: PanelProps) => {
   return (
-    <div
-      className={clsx(
-        'my-4 w-full overflow-hidden rounded-lg border shadow',
-        types[type].border,
-      )}
-    >
-      <Disclosure defaultOpen={true}>
-        {({ open }) => (
-          <>
-            <div
-              className={clsx(
-                'flex items-center p-4',
-                types[type].bg,
-                open && types[type].openText,
-                open ? 'bg-opacity-30' : 'bg-opacity-90',
+    <Draggable draggableId={draggableId} index={draggableIndex}>
+      {(provided, snapshot) => (
+        <div ref={provided.innerRef} {...provided.draggableProps}>
+          <div
+            className={clsx(
+              'my-4 w-full overflow-hidden rounded-lg border shadow',
+              types[type].border,
+            )}
+          >
+            <Disclosure defaultOpen={true}>
+              {({ open }) => (
+                <>
+                  <div
+                    className={clsx(
+                      'flex items-center p-4',
+                      types[type].bg,
+                      open && types[type].openText,
+                      open ? 'bg-opacity-30' : 'bg-opacity-90',
+                    )}
+                  >
+                    <Disclosure.Button>
+                      <ChevronUpIcon
+                        className={`mr-2 h-5 w-5 ${
+                          !open && 'rotate-180'
+                        } transition-all`}
+                      />
+                    </Disclosure.Button>
+                    <span className="flex-1 font-semibold">Panel Header</span>
+                    <div {...provided.dragHandleProps}>
+                      <MenuIcon className="mx-2 h-5 w-5" />
+                    </div>
+                    <TrashIcon className="h-5 w-5" />
+                  </div>
+                  <Transition
+                    show={open}
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+                  >
+                    <Disclosure.Panel static className="p-4">
+                      {children}
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
               )}
-            >
-              <Disclosure.Button>
-                <ChevronUpIcon
-                  className={`mr-2 h-5 w-5 ${
-                    !open && 'rotate-180'
-                  } transition-all`}
-                />
-              </Disclosure.Button>
-              <span className="flex-1 font-semibold">Panel Header</span>
-              <MenuIcon className="mx-2 h-5 w-5" />
-              <TrashIcon className="h-5 w-5" />
-            </div>
-            <Transition
-              show={open}
-              enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
-            >
-              <Disclosure.Panel static className="p-4">
-                {children}
-              </Disclosure.Panel>
-            </Transition>
-          </>
-        )}
-      </Disclosure>
-    </div>
+            </Disclosure>
+          </div>
+        </div>
+      )}
+    </Draggable>
   )
 }
 
--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -1,22 +1,31 @@
-import { Button } from '@/components/Elements/Button'
-import { Spacer } from '@/components/Elements/Spacer'
 import type { NextPage } from 'next'
-import { ArrowRightIcon } from '@heroicons/react/solid'
-import Link from 'next/link'
 import Panel from '@/components/Panel'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const panels = []
 
 const Studio: NextPage = () => {
   return (
     <div>
       <h1 className="p-2 text-center text-6xl font-bold">Studio</h1>
-      <Panel type="room">
-        <Panel type="quest">
-          <div>Nested Panel</div>
-        </Panel>
-      </Panel>
-      <Panel>
-        <span>Lorem ipsum</span>
-      </Panel>
+      <DragDropContext onDragEnd={result => console.log(result)}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Panel type="room" draggableId="abc" draggableIndex={1}>
+                <Panel type="quest" draggableId="ghi" draggableIndex={3}>
+                  <div>Nested Panel</div>
+                </Panel>
+              </Panel>
+              <Panel draggableId="def" draggableIndex={2} key="def">
+                <span>Lorem ipsum</span>
+              </Panel>
+
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
