b476c15b0c5b515a50996478e7c1d99bd978ab34
Felix Erdmann <hello@felixerdmann.com>
2022-04-21 09:18:29 +0200

    update structure of panels

--- components/Panel/index.tsx
+++ components/Panel/index.tsx
@@ -1,16 +1,26 @@
 import { Disclosure, Transition } from '@headlessui/react'
 import { ChevronUpIcon, MenuIcon, TrashIcon } from '@heroicons/react/outline'
 import clsx from 'clsx'
-import { Draggable } from 'react-beautiful-dnd'
+import { Draggable, DraggableProvided } from 'react-beautiful-dnd'
 import { resetServerContext } from 'react-beautiful-dnd'
 
 resetServerContext()
 
-type PanelProps = {
+type BasePanelProps = {
   children: React.ReactElement
   type?: keyof typeof types
-  draggableId: string
-  draggableIndex: number
+  header: string
+}
+
+type PanelProps = BasePanelProps & {
+  draggable?: {
+    draggableId: string
+    draggableIndex: number
+  }
+}
+
+type PanelWithOptionalDragHandleProps = BasePanelProps & {
+  provided?: DraggableProvided
 }
 
 const types = {
@@ -26,63 +36,85 @@ const types = {
   },
 }
 
-const Panel = ({
+const PanelWithOptionalDragHandle = ({
   children,
   type = 'room',
-  draggableId,
-  draggableIndex,
-}: PanelProps) => {
-  return (
-    <Draggable draggableId={draggableId} index={draggableIndex}>
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.draggableProps}>
+  provided,
+  header,
+}: PanelWithOptionalDragHandleProps) => (
+  <div
+    className={clsx(
+      'my-4 w-full overflow-hidden rounded-lg border bg-black shadow',
+      types[type].border,
+    )}
+  >
+    <Disclosure defaultOpen={true}>
+      {({ open }) => (
+        <>
           <div
             className={clsx(
-              'my-4 w-full overflow-hidden rounded-lg border shadow',
-              types[type].border,
+              'flex items-center p-4',
+              types[type].bg,
+              open && types[type].openText,
+              open ? 'bg-opacity-30' : 'bg-opacity-90',
             )}
           >
-            <Disclosure defaultOpen={true}>
-              {({ open }) => (
-                <>
-                  <div
-                    className={clsx(
-                      'flex items-center p-4',
-                      types[type].bg,
-                      open && types[type].openText,
-                      open ? 'bg-opacity-30' : 'bg-opacity-90',
-                    )}
-                  >
-                    <Disclosure.Button>
-                      <ChevronUpIcon
-                        className={`mr-2 h-5 w-5 ${
-                          !open && 'rotate-180'
-                        } transition-all`}
-                      />
-                    </Disclosure.Button>
-                    <span className="flex-1 font-semibold">Panel Header</span>
-                    <div {...provided.dragHandleProps}>
-                      <MenuIcon className="mx-2 h-5 w-5" />
-                    </div>
-                    <TrashIcon className="h-5 w-5" />
-                  </div>
-                  <Transition
-                    show={open}
-                    enter="transition duration-100 ease-out"
-                    enterFrom="transform scale-95 opacity-0"
-                    enterTo="transform scale-100 opacity-100"
-                    leave="transition duration-75 ease-out"
-                    leaveFrom="transform scale-100 opacity-100"
-                    leaveTo="transform scale-95 opacity-0"
-                  >
-                    <Disclosure.Panel static className="p-4">
-                      {children}
-                    </Disclosure.Panel>
-                  </Transition>
-                </>
-              )}
-            </Disclosure>
+            <Disclosure.Button>
+              <ChevronUpIcon
+                className={`mr-2 h-5 w-5 ${
+                  !open && 'rotate-180'
+                } transition-all`}
+              />
+            </Disclosure.Button>
+            <span className="flex-1 font-semibold">{header}</span>
+            {provided && (
+              <div {...provided.dragHandleProps}>
+                <MenuIcon className="mx-2 h-5 w-5" />
+              </div>
+            )}
+            <TrashIcon className="h-5 w-5" />
           </div>
+          <Transition
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel static className="p-4">
+              {children}
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
+  </div>
+)
+
+const Panel = ({ children, type = 'room', draggable, header }: PanelProps) => {
+  if (!draggable)
+    return (
+      <PanelWithOptionalDragHandle type={type} header={header}>
+        {children}
+      </PanelWithOptionalDragHandle>
+    )
+
+  return (
+    <Draggable
+      draggableId={draggable.draggableId}
+      index={draggable.draggableIndex}
+    >
+      {(provided, snapshot) => (
+        <div ref={provided.innerRef} {...provided.draggableProps}>
+          <PanelWithOptionalDragHandle
+            type={type}
+            provided={provided}
+            header={header}
+          >
+            {children}
+          </PanelWithOptionalDragHandle>
         </div>
       )}
     </Draggable>
--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -12,12 +12,25 @@ const Studio: NextPage = () => {
         <Droppable droppableId="droppable">
           {(provided, snapshot) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Panel type="room" draggableId="abc" draggableIndex={1}>
-                <Panel type="quest" draggableId="ghi" draggableIndex={3}>
+              <Panel
+                type="room"
+                draggable={{
+                  draggableId: 'abc',
+                  draggableIndex: 1,
+                }}
+                header="Raum 1"
+              >
+                <Panel type="quest" header="Rätsel 1">
                   <div>Nested Panel</div>
                 </Panel>
               </Panel>
-              <Panel draggableId="def" draggableIndex={2} key="def">
+              <Panel
+                draggable={{
+                  draggableId: 'def',
+                  draggableIndex: 2,
+                }}
+                header="Raum 2"
+              >
                 <span>Lorem ipsum</span>
               </Panel>
 
