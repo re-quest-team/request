0c96166bfa6b5ec8ca8310f85eae2bc5364ec246
Felix Erdmann <hello@felixerdmann.com>
2022-04-21 11:14:36 +0200

    initial next-auth setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import { SessionProvider } from 'next-auth/react'
 
 import '@fontsource/inter'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/900.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <div className="h-full bg-black text-white">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <SessionProvider session={session}>
+      <div className="h-full bg-black text-white">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </SessionProvider>
   )
 }
 
