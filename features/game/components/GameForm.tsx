d94fa3f53d3923fd59b45535235839c8084d6f35
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-06-02 19:50:22 +0200

    translated the Game Form

--- features/game/components/GameForm.tsx
+++ features/game/components/GameForm.tsx
@@ -10,6 +10,7 @@ import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import useSWR from 'swr'
 import * as yup from 'yup'
+import { FormattedMessage, useIntl } from 'react-intl'
 
 type GameFormProps = {
   id: string
@@ -24,13 +25,17 @@ const schema = yup
   .required()
 
 const GameForm = ({ id }: GameFormProps) => {
+  const intl = useIntl()
+
   const { data, mutate, isValidating, error } = useSWR<Game, AxiosError>(
     `/api/game/${id}`,
   )
   const router = useRouter()
 
   if (error && error.response?.status === 404) {
-    toast.error('re:quest nicht gefunden')
+    toast.error(
+      intl.formatMessage({ id: 'features.game.gameForm.toastNotFound' }),
+    )
     router.replace('/studio')
   }
 
@@ -49,10 +54,14 @@ const GameForm = ({ id }: GameFormProps) => {
       },
     })
 
+    const msg1 = intl.formatMessage({ id: 'toasts.create.loading' })
+    const msg2 = intl.formatMessage({ id: 'toasts.create.success' })
+    const msg3 = intl.formatMessage({ id: 'toasts.create.error' })
+
     toast.promise(postGameRequest, {
-      loading: 'Speichern',
-      success: 'Erfolgreich gespeichert',
-      error: 'Fehler beim Speichern',
+      loading: msg1,
+      success: msg2,
+      error: msg3,
     })
     const updatedGameRequest = await postGameRequest
 
@@ -61,30 +70,36 @@ const GameForm = ({ id }: GameFormProps) => {
     await mutate(updatedGame)
   })
 
+  const label1 = intl.formatMessage({ id: 'features.game.gameForm.labelName' })
+  const label2 = intl.formatMessage({
+    id: 'features.game.gameForm.labelDescription',
+  })
+  const label3 = intl.formatMessage({ id: 'features.game.gameForm.labelDraft' })
+
   return (
     <form onSubmit={onSubmit}>
       <InputField
-        label="Name"
+        label={label1}
         defaultValue={data?.name ?? ''}
         registration={register('name')}
         error={errors['name']}
       ></InputField>
       <TextArea
-        label="Beschreibung"
+        label={label2}
         rows={4}
         defaultValue={data?.description ?? ''}
         registration={register('description')}
         error={errors['description']}
       />
       <Toggle
-        label="Entwurf"
+        label={label3}
         defaultChecked={data?.draft}
         registration={register('draft')}
         error={errors['draft']}
       />
 
       <Button type="submit" disabled={isValidating} isLoading={isValidating}>
-        Speichern
+        <FormattedMessage id="features.game.gameForm.submit" />
       </Button>
     </form>
   )
