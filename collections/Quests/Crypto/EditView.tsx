c5e9cefea076488a1b84730bad9f385d1416a798
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-06-02 19:41:55 +0200

    translated the Crypto Edit View

--- collections/Quests/Crypto/EditView.tsx
+++ collections/Quests/Crypto/EditView.tsx
@@ -1,7 +1,18 @@
 import { InputField } from '@/components/Elements/FormElements'
 import { useQuestStore } from './store'
+import { useIntl } from 'react-intl'
 
 const EditView = () => {
+  const intl = useIntl()
+
+  const label1 = intl.formatMessage({ id: 'quests.crypto.editView.labelTask' })
+  const label2 = intl.formatMessage({
+    id: 'quests.crypto.editView.labelCodeWord',
+  })
+  const label3 = intl.formatMessage({
+    id: 'quests.crypto.editView.labelEncryptedWord',
+  })
+
   const question = useQuestStore(state => state.question)
   const setQuestion = useQuestStore(state => state.setQuestion)
 
@@ -11,17 +22,17 @@ const EditView = () => {
   return (
     <div>
       <InputField
-        label="Aufgabenstellung"
+        label={label1}
         defaultValue={question}
         onChange={e => setQuestion(e.target.value)}
       ></InputField>
       <InputField
-        label="Codewort"
+        label={label2}
         defaultValue={codeword}
         onChange={e => setCordeword(e.target.value)}
       ></InputField>
       <InputField
-        label="Verschlüsseltes Wort"
+        label={label3}
         disabled
         value={codeword.replace(
           /[A-Z]/gi,
