5ccdd5491dfb4f9596985e6cc1033459a454c895
Nikola Pavlovic <pavlovic.nikola@aol.de>
2022-06-04 04:06:27 +0200

    Implemented png and svg download

--- features/game/components/GamePanel.tsx
+++ features/game/components/GamePanel.tsx
@@ -1,13 +1,12 @@
 import { Button } from '@/components/Elements/Button'
-import Select from '@/components/Elements/Select'
+import Select, { SelectOption } from '@/components/Elements/Select'
 import Panel from '@/components/Panel'
 import { Game } from '@prisma/client'
 import Link from 'next/link'
-import { useEffect, createRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useSWRConfig } from 'swr'
 import { deleteGame } from '../api/deleteGame'
-import { SelectOption } from '@/components/Elements/Select'
 import { DocumentDownloadIcon } from '@heroicons/react/solid'
 import QRCodeStyling from 'qr-code-styling'
 
@@ -17,24 +16,41 @@ const qrOptions: SelectOption[] = [
   { value: 'PDF' },
 ]
 
-const qrCode = new QRCodeStyling({
-  width: 500,
-  height: 500,
-  dotsOptions: {
-    color: 'rgb(24 24 27)',
-    type: 'rounded',
-  },
-  imageOptions: {
-    hideBackgroundDots: false,
-    margin: 20,
-  },
-})
+const QRCode = (url: string, imageUrl: string) => {
+  return new QRCodeStyling({
+    width: 1000,
+    height: 1000,
+    dotsOptions: {
+      //color: 'rgb(24 24 27)',
+      gradient: {
+        type: 'linear',
+        rotation: 45,
+        colorStops: [
+          { offset: 0, color: '#622d28' },
+          { offset: 1, color: '#6d4072' },
+        ],
+      },
+      type: 'rounded',
+    },
+    imageOptions: {
+      imageSize: 0.2,
+      margin: 15,
+    },
+    data: url,
+    image: imageUrl,
+  })
+}
 
 const GamePanel = ({ id, name, description }: Game) => {
   const { mutate } = useSWRConfig()
+
   const [hostname, setHostname] = useState('')
-  const [fileExt, setFileExt] = useState(qrOptions[0])
-  const qrRef = createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHostname(window.location.origin)
+    }
+  }, [])
 
   const onDelete = async () => {
     const deleteGameRequest = deleteGame(id)
@@ -51,6 +67,13 @@ const GamePanel = ({ id, name, description }: Game) => {
     })
   }
 
+  const [fileExt, setFileExt] = useState(qrOptions[0])
+
+  const qrCode = QRCode(
+    `${hostname}/play/${id}`,
+    'https://raw.githubusercontent.com/re-quest-team/request/e288576a3778a34b817cf01e9ca2398e1c40ebd2/assets/logos/request-logo-single.svg',
+  )
+
   const onDownload = () => {
     qrCode.download({
       // @ts-ignore
@@ -59,41 +82,30 @@ const GamePanel = ({ id, name, description }: Game) => {
   }
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setHostname(window.location.origin)
-    }
-  }, [])
-
-  useEffect(() => {
-    // @ts-ignore
-    qrCode.append(qrRef.current)
-  }, [qrRef])
-
-  useEffect(() => {
-    // Todo only the last qr code gets used
     qrCode.update({
       data: `${hostname}/play/${id}`,
-      // TODO proper way to get the image url. It MUST be a url....
+      // proper way to get the image url. MUST be an url.
       image:
         'https://raw.githubusercontent.com/re-quest-team/request/e288576a3778a34b817cf01e9ca2398e1c40ebd2/assets/logos/request-logo-single.svg',
     })
-  }, [hostname, id])
+  }, [hostname, id, qrCode])
 
   return (
     <Panel type="quest" header={name || ''} onDelete={onDelete}>
       <>
         <div className="relative my-4 w-full rounded">
-          <div className="flex w-full flex-col justify-between">
+          <div className="flex w-full flex-col">
             <div>
               <p>{description}</p>
             </div>
-            <div className="flex w-full flex-row">
+            <div className="mt-4 flex flex-row">
               <Link href={`/studio/${id}`} passHref>
                 <Button>Bearbeiten</Button>
               </Link>
               <div className="flex w-full flex-row justify-end">
-                <Select options={qrOptions} onSelect={setFileExt} />
-
+                <div className="mb-1">
+                  <Select options={qrOptions} onSelect={setFileExt} />
+                </div>
                 <Button
                   onClick={onDownload}
                   endIcon={<DocumentDownloadIcon className="h-4" />}
