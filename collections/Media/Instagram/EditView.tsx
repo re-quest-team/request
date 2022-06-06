5e1e701d032a6c67c1019c581d4350ec6d450dec
Thomas Rottenfusser <thomas.rottenfusser@hm.edu>
2022-06-07 01:30:42 +0200

    Fix: PlayView change script to iframe

--- collections/Media/Instagram/EditView.tsx
+++ collections/Media/Instagram/EditView.tsx
@@ -1,18 +1,18 @@
 import { useInstagramStore } from './store'
-import {TextArea} from "@/components/Elements/FormElements/TextArea";
+import { TextArea } from '@/components/Elements/FormElements/TextArea'
 
 const EditView = () => {
   const link = useInstagramStore(state => state.link)
   const setLink = useInstagramStore(state => state.setLink)
   return (
-          <div>
-            <TextArea
-                label="Add Instagram Embed-link:"
-                defaultValue={link}
-                onChange={e => setLink(e.target.value)}
-                rows={4}
-            ></TextArea>
-          </div>
+    <div>
+      <TextArea
+        label="Add Instagram Embed-link:"
+        defaultValue={link}
+        onChange={e => setLink(e.target.value)}
+        rows={4}
+      ></TextArea>
+    </div>
   )
 }
 
--- collections/Media/Instagram/PlayView.tsx
+++ collections/Media/Instagram/PlayView.tsx
@@ -1,53 +1,20 @@
-import Script from 'next/script'
 import { useInstagramStore } from './store'
 
 // const link = "https://www.instagram.com/p/CeNcSPaq9Vt/?utm_source=ig_embed&amp;utm_campaign=loading";
 
-const blockquoteStyle = {
-    background: '#fff',
-    border: '0',
-    borderRadius: '3px',
-    boxShadow: '0 0 1px 0 rgba(0, 0, 0, 0.5), 0 1px 10px 0 rgba(0, 0, 0, 0.15)',
-    margin: '1px',
-    maxWidth: '540px',
-    minWidth: '326px',
-    padding: '0',
-    width: '99.375%',
-
-};
-
 const div = {
-    color: '#100e0e',
-    fontFamily: 'Arial, sans-serif',
-    textAlign: "center" as "center",
-    fontSize: '14px',
-    fontStyle: 'normal',
-    fontWeight: '550',
-    lineHeight: '18px',
+  textAlign: 'center' as 'center',
 }
-
-
 
 const PlayView = () => {
-  const link = useInstagramStore(state => state.link)
-  const scriptSrc = "http://www.instagram.com/embed.js"
-  return(
-        <div>
-            <blockquote
-                className="instagram-media"
-                data-instgrm-permalink={link}
-                data-instgrm-version="14"
-                style={blockquoteStyle}
-                >
-                <div
-                    style={div}
-                >
-                    error 404
-                </div>
-            </ blockquote>
-            <Script src={scriptSrc}></Script>
-        </div>
-  )
-}
+  const link = useInstagramStore(state => state.link) + "/embed";
+  return (
+      <div style={div}>
+        <iframe width="500" height="870" src={link} frameBorder="0" >
+          <div style={div}>error 404</div>
+        </iframe>
+      </div>
+  );
 
+}
 export default PlayView
