3174c5f01a7d2dd1e170bf2a29cfcefd07fbe2e8
Felix Erdmann <hello@felixerdmann.com>
2022-06-17 16:12:44 +0200

    update instagram layout

--- collections/Media/Instagram/PlayView.tsx
+++ collections/Media/Instagram/PlayView.tsx
@@ -4,7 +4,12 @@ const PlayView = () => {
   const link = useInstagramStore(state => state.link) + '/embed'
   return (
     <div className="text-center">
-      <iframe width="500" height="870" src={link} frameBorder="0">
+      <iframe
+        className="relative w-full overflow-hidden pr-12"
+        height="480"
+        src={link}
+        frameBorder="0"
+      >
         <div className="text-center">error 404</div>
       </iframe>
     </div>
