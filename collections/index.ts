a31220796e8ec2185f1ce97a8b17fbeb13cffdd7
AbtinDarbandy <43971114+AbtinDarbandy@users.noreply.github.com>
2022-06-21 15:50:41 +0200

    merge

--- collections/index.ts
+++ collections/index.ts
@@ -7,11 +7,13 @@ import InstagramMedia from '@/collections/Media/Instagram'
 import NumberInputQuest from './Quests/NumberInput'
 import { IntlShape } from 'react-intl'
 import { IQuest } from '@/collections/types'
+import IframeMedia from '@/collections/Media/IFrame'
 
 const media = (intl: IntlShape): IQuest<any>[] => [
   TextMedia(intl),
   InstagramMedia(intl),
   YoutubeMedia(intl),
+  IframeMedia(intl),
 ]
 const quests = (intl: IntlShape): IQuest<any>[] => [
   CryptoQuest(intl),
