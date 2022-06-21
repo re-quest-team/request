04e69bbaf6b24fdca864535d2f81c7e91daf93e3
AbtinDarbandy <43971114+AbtinDarbandy@users.noreply.github.com>
2022-06-21 19:38:27 +0200

    removed console logs

--- utils/compareStringArray.ts
+++ utils/compareStringArray.ts
@@ -1,6 +1,4 @@
 export const compareStringArray = (array1: string[], array2: string[]) => {
-  console.log(array1)
-  console.log(array2)
   if (array1.length != array2.length) return false
   for (let i = 0, l = array1.length; i < l; i++) {
     if (!(array1[i] === array2[i])) return false
