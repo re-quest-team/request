7536acecba6c3708f456cca283c69e4d6867e760
Koki <geiger.konstantin@hm.edu>
2022-06-21 12:47:01 +0200

    finished custom block but still not showing grid nor toolbox for some reason

--- collections/Quests/Blockly/PlayView.tsx
+++ collections/Quests/Blockly/PlayView.tsx
@@ -1,12 +1,7 @@
 import './customBlocks/customBlocks'
-import './Test'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Blockly from 'blockly'
-import { BlocklyWorkspace } from 'react-blockly'
-import { Simulate } from 'react-dom/test-utils'
-import error = Simulate.error
-import { Button } from '@/components/Elements/Button'
-import { Test } from '@/collections/Quests/Blockly/Test'
+import { BlocklyWorkspace, useBlocklyWorkspace } from 'react-blockly'
 
 const PlayView = () => {
   const [xml, setXml] = useState('')
@@ -14,50 +9,40 @@ const PlayView = () => {
 
   const initialXml =
     '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>'
+
   const toolboxCategories = {
     kind: 'categoryToolbox',
     contents: [
       {
         kind: 'category',
-        name: 'Logic',
+        name: 'Custom',
         colour: '#5C81A6',
         contents: [
           {
             kind: 'block',
-            type: 'controls_if',
+            type: 'turn_angle',
           },
           {
             kind: 'block',
-            type: 'logic_compare',
-          },
-        ],
-      },
-      {
-        kind: 'category',
-        name: 'Math',
-        colour: '#5CA65C',
-        contents: [
-          {
-            kind: 'block',
-            type: 'math_round',
-          },
-          {
-            kind: 'block',
-            type: 'math_number',
+            type: 'direction',
           },
         ],
       },
     ],
   }
+
   function workspaceDidChange(workspace) {
     const code = Blockly.JavaScript.workspaceToCode(workspace)
     setJavascriptCode(code)
   }
 
+  const editorDiv = React.useRef(null)
+
   return (
     <div>
       <div>test</div>
       <BlocklyWorkspace
+        ref={editorDiv}
         toolboxConfiguration={toolboxCategories}
         initialXml={initialXml}
         className="fill-height"
@@ -73,7 +58,7 @@ const PlayView = () => {
         onXmlChange={setXml}
       />
       <div>test</div>
-      <pre id="generated-xml">{xml}</pre>
+      {/*<pre id="generated-xml">{xml}</pre>*/}
       <textarea
         id="code"
         style={{ height: '600px', width: '400px' }}
