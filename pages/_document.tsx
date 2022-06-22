caf3990acdec96e30d92e3f7e2546936dd49f1f1
Felix Erdmann <hello@felixerdmann.com>
2022-06-22 11:36:41 +0200

    add umami tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
     <Html className="bg-zinc-900">
       <Head>
         <link rel="icon" href="/favicon.svg" />
+        <script
+          async
+          defer
+          data-website-id="841fe53d-0f98-4373-a901-76a9337432fe"
+          src="https://umami-eta-one.vercel.app/umami.js"
+        ></script>
       </Head>
       <body>
         <Main />
